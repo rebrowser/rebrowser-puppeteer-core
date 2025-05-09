33f5ddd4e9ec4a4338930e3ef0dbcd75e064e80d
nwebson <nwebson@outlook.com>
2025-05-08 17:46:01 -0700

    original

---
+++ lib/cjs/puppeteer/generated/version.d.ts
@@ -0,0 +1,5 @@
+/**
+ * @internal
+ */
+export declare const packageVersion = "24.8.1";
+//# sourceMappingURL=version.d.ts.map
---
+++ lib/esm/puppeteer/revisions.d.ts
@@ -0,0 +1,14 @@
+/**
+ * @license
+ * Copyright 2020 Google Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+/**
+ * @internal
+ */
+export declare const PUPPETEER_REVISIONS: Readonly<{
+    chrome: "136.0.7103.49";
+    'chrome-headless-shell': "136.0.7103.49";
+    firefox: "stable_138.0.1";
+}>;
+//# sourceMappingURL=revisions.d.ts.map
